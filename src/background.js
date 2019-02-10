8440dc626e251f5c335a113aa9f113b9cc927057
Cheesestringer <4462478+cheesestringer@users.noreply.github.com>
2019-02-10 18:56:35 +1100

    init

---
+++ src/background.js
@@ -0,0 +1,183 @@
+const searchUrl = new URL("https://chalice-search-api.cloud.seek.com.au/search");
+
+const calculateRange = async jobId => {
+  const minRange = 30000;
+  const maxRange = 200000;
+
+  const maxSalary = await getMaxSalary(jobId, minRange, maxRange);
+  const minSalary = await getMinSalary(jobId, minRange, maxSalary);
+
+  console.log(`Salary range is $${minSalary} - $${maxSalary}`);
+
+  if (minSalary && maxSalary) {
+    return `$${minSalary.toLocaleString()} - $${maxSalary.toLocaleString()}`;
+  }
+};
+
+const getMaxSalary = async (jobId, min, max) => {
+  let minimum = min;
+  let maximum = max;
+  let searchValue = getMiddle(minimum, maximum);
+
+  // Limit to 10 requests
+  for (let i = 0; i < 10; i++) {
+    const job = await getJob(jobId, searchValue, maximum);
+    if (job && job.found) {
+      minimum = searchValue;
+      searchValue = getMiddle(searchValue, maximum);
+
+      // Check percentage change and round up to the nearest 1k to save on requests.
+      if (buggerAllChange(minimum, searchValue)) {
+        console.log(`Job found: Max range ${minimum}-${searchValue} found after ${i + 1} requests.`);
+        return roundUp(searchValue);
+      }
+    } else {
+      maximum = searchValue;
+      searchValue = getMiddle(minimum, searchValue );
+
+      // Check percentage change and round up to the nearest 1k to save on requests.
+      if (buggerAllChange(searchValue, maximum)) {
+        console.log(`Job missing: Max range ${searchValue}-${maximum} found after ${i + 1} requests.`);
+        return roundUp(searchValue);
+      }
+    }
+  }
+
+  console.log("Max salary not found after 10 requests.");
+  return roundUp(searchValue);
+};
+
+const getMinSalary = async (jobId, min, max) => {
+  let minimum = min;
+  let maximum = max;
+  let searchValue = getMiddle(minimum, maximum);
+
+  // Limit to 10 requests
+  for (let i = 0; i < 10; i++) {
+    const job = await getJob(jobId, minimum, searchValue);
+    if (job && job.found) {
+      maximum = searchValue;
+      searchValue = getMiddle(minimum, searchValue);
+
+      // Check percentage change and round down to the nearest 1k to save on requests.
+      if (buggerAllChange(searchValue, maximum)) {
+        console.log(`Job found: Min ${searchValue}-${maximum} found after ${i + 1} requests.`);
+        return roundDown(maximum);
+      }
+    } else {
+      minimum = searchValue;
+      searchValue = getMiddle(searchValue, maximum);
+
+      // Check percentage change and round down to the nearest 1k to save on requests.
+      if (buggerAllChange(minimum, searchValue)) {
+        console.log(`Job missing: Min ${minimum}-${searchValue} found after ${i + 1} requests.`);
+        return roundDown(searchValue);
+      }
+    }
+  }
+
+  console.log("Min salary not found after 10 requests.");
+  return roundDown(searchValue);
+};
+
+const getMiddle = (lower, upper) => {
+  return Math.round((lower + upper) / 2);
+};
+
+const buggerAllChange = (first, second) => {
+  return (first / second) * 100 > 96;
+};
+
+const roundUp = value => {
+  return Math.ceil(value / 1000) * 1000;
+};
+
+const roundDown = value => {
+  return Math.floor(value / 1000) * 1000;
+};
+
+const getJobId = url => {
+  try {
+    return new URL(url).pathname.split("/")[2];
+  } catch {
+    return "";
+  }
+};
+
+const getJob = async (jobId, min, max) => {
+  searchUrl.searchParams.set("jobid", jobId);
+  searchUrl.searchParams.set("salaryrange", `${min}-${max}`);
+  const response = await fetch(searchUrl.href);
+  if (response.status === 200) {
+    const result = await response.json();
+
+    if (result && result.data && result.data.length === 1) {
+      return { found: true };
+    } else {
+      return { found: false };
+    }
+  } else {
+    sendMessage(`Error: Request failed with ${response.status}`);
+    throw new Error(`Unsuccessful response: ${response.status}`);
+  }
+};
+
+const sendMessage = result => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      message: "updateSalary",
+      result: result
+    });
+  });
+};
+
+const handleScriptInjection = (tabId, url) => {
+  chrome.tabs.executeScript(
+    tabId,
+    {
+      code: "var injected = window.seekerInjected; window.seekerInjected = true; injected;"
+    },
+    async response => {
+      // Seeker is already injected.
+      if (response[0]) {
+        chrome.tabs.executeScript(tabId, {
+          code: "addPlaceholder();"
+        });
+      } else {
+        chrome.tabs.executeScript(tabId, {
+          file: "seeker.js"
+        });
+      }
+
+      const jobId = getJobId(url);
+      if (jobId) {
+        const salary = await calculateRange(jobId);
+        sendMessage(salary);
+      }
+    }
+  );
+};
+
+const isJobUrl = url => url.includes("/job/");
+
+// Handle job access by site navigation.
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status !== "loading" || !changeInfo.url) {
+    return;
+  }
+
+  if (isJobUrl(changeInfo.url)) {
+    handleScriptInjection(tabId, changeInfo.url);
+  }
+});
+
+// Handle job access by direct URL.
+chrome.webNavigation.onCommitted.addListener(details => {
+  if (!details.tabId || !details.url) {
+    return;
+  }
+
+  if (isJobUrl(details.url)) {
+    handleScriptInjection(details.tabId, details.url);
+  }
+});
---
+++ src/seeker.js
@@ -0,0 +1,46 @@
+const selectors = {
+  salaryRange: "salary-range-value"
+};
+
+const addPlaceholder = () => {
+  const infoSections = document.querySelectorAll(
+    '[aria-labelledby="jobInfoHeader"]'
+  );
+
+  for (const section of infoSections) {
+    const items = section.querySelectorAll("dd");
+    const salaryRange = items[1].cloneNode(true);
+    salaryRange.querySelector("strong").innerText = "Salary";
+
+    const salaryRangePlaceholder = salaryRange.querySelector("span span span");
+    if (salaryRangePlaceholder) {
+      salaryRangePlaceholder.id = selectors.salaryRange;
+      salaryRangePlaceholder.innerText = "Calculating...";
+    } else {
+      const span = document.createElement("span");
+      span.id = selectors.salaryRange;
+      span.innerText = "Calculating...";
+      span.style.fontSize = "14px";
+      salaryRange.appendChild(span);
+    }
+
+    items[1].parentNode.insertBefore(salaryRange, items[1].nextSibling);
+  }
+};
+
+const updatePlaceholder = text => {
+  const elements = document.querySelectorAll(`#${selectors.salaryRange}`);
+  for (const element of elements) {
+    element.innerText = text;
+  }
+};
+
+addPlaceholder();
+
+chrome.runtime.onMessage.addListener(request => {
+  if (request.message === "updateSalary") {
+    request.result
+      ? updatePlaceholder(request.result)
+      : updatePlaceholder("Error downloading salary.");
+  }
+});
