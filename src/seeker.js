dfa5ff180308c0f7cf7a9c3097c4ceb7107c127d
Simon <4462478+cheesestringer@users.noreply.github.com>
2021-04-25 17:27:33 +1000

    Add support for the new seek experience
    Fixes https://github.com/cheesestringer/salary-seeker/issues/16

--- src/seeker.js
+++ src/seeker.js
@@ -1,8 +1,40 @@
 const selectors = {
-  salaryRange: "salary-range-value"
+  salaryRange: "salary-range-value",
+  legacySalaryRange: "legacy-salary-range-value",
 };
 
-const addPlaceholder = () => {
+const addPlaceholder = (text) => {
+  const elements = document.querySelectorAll("span");
+  for (const element of elements) {
+    if (element.innerText.includes('Posted')) {
+      const div = document.createElement("div");
+      div.style.marginTop = "10px";
+
+      const span = document.createElement("span");
+      span.id = selectors.salaryRange;
+      span.innerText = `Salary (estimated): ${text}`;
+      span.style.fontSize = "16px";
+      span.style.lineHeight = "24px";
+
+      div.append(span);
+      element.parentElement.before(div);
+    }
+  }
+};
+
+// Seek seems to be doing a/b testing so try and support both for now
+const showSalary = async (value) => {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  try {
+    addPlaceholder(value);
+  } catch (exception) { }
+  try {
+    updateLegacyPlaceholder(value);
+  } catch (exception) { }
+}
+
+const addLegacyPlaceholder = () => {
   const infoSections = document.querySelectorAll('[aria-labelledby="jobInfoHeader"]');
   for (const section of infoSections) {
     const items = section.querySelectorAll("dd");
@@ -11,11 +43,11 @@ const addPlaceholder = () => {
 
     const salaryRangePlaceholder = salaryRange.querySelector("span span span");
     if (salaryRangePlaceholder) {
-      salaryRangePlaceholder.id = selectors.salaryRange;
+      salaryRangePlaceholder.id = selectors.legacySalaryRange;
       salaryRangePlaceholder.innerText = "Calculating...";
     } else {
       const span = document.createElement("span");
-      span.id = selectors.salaryRange;
+      span.id = selectors.legacySalaryRange;
       span.innerText = "Calculating...";
       span.style.fontSize = "14px";
       salaryRange.appendChild(span);
@@ -25,16 +57,16 @@ const addPlaceholder = () => {
   }
 };
 
-const updatePlaceholder = (text) => {
+const updateLegacyPlaceholder = (text) => {
   // Wait for a max of 2 seconds for career insights to load before adding the placeholder.
   let elapsed = 0;
   const interval = setInterval(() => {
     const insights = document.querySelector("div[data-automation='dynamic-lmis']");
     if (insights || elapsed >= 2000) {
       clearInterval(interval);
-      addPlaceholder();
+      addLegacyPlaceholder();
 
-      const elements = document.querySelectorAll(`#${selectors.salaryRange}`);
+      const elements = document.querySelectorAll(`#${selectors.legacySalaryRange}`);
       for (const element of elements) {
         element.innerText = text;
       }
@@ -46,6 +78,7 @@ const updatePlaceholder = (text) => {
 
 chrome.runtime.onMessage.addListener((request) => {
   if (request.message === "update-placeholder") {
-    request.result ? updatePlaceholder(request.result) : updatePlaceholder("Error downloading salary.");
+    console.log(`Salary range: ${request.result}`);
+    request.result ? showSalary(request.result) : showSalary("Error downloading salary.");
   }
 });
